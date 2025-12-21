8579204a8754e69d8e500b0553e65ceeabb55ffa
VinayakLengare <vinayaklengare23@gmail.com>
2025-12-21 09:11:06 +0530

    Add navbar component with basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
+
 function App() {
   return (
     <div>
+      <Navbar/>
       <Home />
        <About />
        <Projects/>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Vinayak</h2>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
